41308c7f5b18895cd7c176299a074537cef6e154
andreyhitoshi <anddrey.japa@hotmail.com>
2020-11-20 23:04:45 -0300

    Subindo alterações na master

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 
-import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { Formik, Field, ErrorMessage } from 'formik'
 import * as yup from 'yup'
 import axios from 'axios'
 
@@ -9,94 +9,132 @@ import { history } from '../../history'
 
 import './Register.css';
 
-const Register = () => {
-    const handleSubmit = values => {
-        let axiosConfig = {
+export default class Register extends Component {
+
+    handleSubmit = props => {
+
+        var data = JSON.stringify(props)
+        let config = {
+            method: 'post',
+            url: 'http://127.0.0.1:5000/cadastro',
             headers: {
                 'Content-Type': 'application/json'
-            }
-          };
-        axios.post('http://127.0.0.1:5000/cadastro', axiosConfig)
-            .then(resp => {
-                const { data } = resp
-                if (data) {
-                    history.push('/login')
-                }
-            }).catch((err) => {
-                console.log(err);
+            },
+            data: data
+        };
+
+        // if (data) {
+        //     history.push('/login')
+        // }
+
+        axios(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
             })
+            .catch((error) => {
+                console.log(error);
+            });
     }
-    const validations = yup.object().shape({
+
+    validations = yup.object().shape({
         firstName: yup.string().required('Required'),
         lastName: yup.string().required('Required'),
         email: yup.string().email('Invalid email format').required('Required'),
         password: yup.string().min(8).required('Required')
     })
 
-    return (
-        <>
+
+    render() {
+        return (
             <div>
                 <h1>Registro</h1>
                 <Formik
-                    initialValues={{}}
-                    onSubmit={handleSubmit}
+                    initialValues={{
+                        usuario_nome: '',
+                        usuario_sobrenome: '',
+                        usuario_login: '',
+                        usuario_senha: ''
+                    }}
                     validationSchema={validations}
+                    onSubmit={(values, action) => {
+                        alert(JSON.stringify(values, null, 2));
+                        action.setSubmitting(false);
+                    }, 1000}
                 >
-                    <Form className="Register">
-                        <div className="Register-group">
-                            <Field
-                                type='name'
-                                name='firstName'
-                                className="Register-Field"
-                            />
-                            <ErrorMessage
-                                component="span"
-                                name='firstName'
-                                className="Register-Error"
-                            />
-                        </div>
-                        <div className="Register-group">
-                            <Field
-                                type='text'
-                                name='lastName'
-                                className="Register-Field"
-                            />
-                            <ErrorMessage
-                                component="span"
-                                name='lastName'
-                                className="Register-Error"
-                            />
-                        </div>
-                        <div className="Register-group">
-                            <Field
-                                type='email'
-                                name='email'
-                                className="Register-Field"
-                            />
-                             <ErrorMessage
-                                component="span"
-                                name='email'
-                                className="Register-Error"
-                        />
-                        </div>
-                        <div className='Register-control'>
-                            <Field
-                                type='password'
-                                name='password'
-                                className="Register-Field"
-                            />
-                            <ErrorMessage
-                                component="span"
-                                name='password'
-                                className="Register-Error"
-                            />
-                        </div>
-                        <button className="Register-Btn" type='submit'>Registrar</button>
-                    </Form>
-                </Formik>
-            </div>
-        </>
-    )
-}
+                    {props => (
+                        <form className="Register" onSubmit={props.handleSubmit}>
+                            <div className="Register-group">
+                                <Field
+                                    type='name'
+                                    name='usuario_nome'
+                                    className="Register-Field"
+                                    onChange={props.handleChange}
+                                    onBlur={props.handleBlur}
+                                    value={props.values.usuario_nome}
+                                />
+                                <ErrorMessage
+                                    component="span"
+                                    name='firstName'
+                                    className="Register-Error"
+                                />
+                            </div>
+                            <div className="Register-group">
+                                <Field
+                                    type='text'
+                                    name='usuario_sobrenome'
+                                    className="Register-Field"
+                                    onChange={props.handleChange}
+                                    onBlur={props.handleBlur}
+                                    value={props.values.usuario_sobrenome}
+                                />
+                                <ErrorMessage
+                                    component="span"
+                                    name='lastName'
+                                    className="Register-Error"
+                                />
+                            </div>
+                            <div className="Register-group">
+                                <Field
+                                    type='email'
+                                    name='usuario_login'
+                                    className="Register-Field"
+                                    onChange={props.handleChange}
+                                    onBlur={props.handleBlur}
+                                    value={props.values.usuario_login}
+                                />
+                                <ErrorMessage
+                                    component="span"
+                                    name='email'
+                                    className="Register-Error"
+                                />
+                            </div>
+                            <div className='Register-control'>
+                                <Field
+                                    type='password'
+                                    name='usuario_senha'
+                                    className="Register-Field"
+                                    onChange={props.handleChange}
+                                    onBlur={props.handleBlur}
+                                    value={props.values.usuario_senha}
+                                />
+                                <ErrorMessage
+                                    component="span"
+                                    name='password'
+                                    className="Register-Error"
+                                />
+                            </div>
+                            <button
+                                type="button"
+                                className="Register-Btn"
+                                onClick={async () => {
 
-export default Register;
+                                }}
+                            >Registrar
+                             </button>
+                        </form>
+                    )}
+                </Formik>
+            </div >
+        );
+    }
+}
